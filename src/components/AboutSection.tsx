1c242d7db92d7e8861870ffcb54ecb5c4d2be392
DonkeykongM <mathias_bahko@hotmail.com>
2025-09-07 21:28:28 +0200

    Fix AboutSection syntax error

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -104,10 +104,3 @@ export const AboutSection: React.FC = () => {
             <p className="text-sm text-gray-500 pt-4 text-center">
               {t.about.since}
             </p>
-        </div>
-      </div>
-    </section>
-  );
-};
-  )
-}
